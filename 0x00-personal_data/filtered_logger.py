65190809c2fca70103c1e32b2ff38d6d5a04aa0b
Peter Boakye <petboa123@gmail.com>
2023-11-03 19:30:58 +0000

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,74 +1,81 @@
 #!/usr/bin/env python3
-"""regex-ing"""
-
-import re
+"""
+This module contains the filter logging methods.
+"""
 import logging
-import os
 import mysql.connector
+import os
+import re
 from typing import List
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ Returns a log message obfuscated """
-    for f in fields:
-        message = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
+    """Filter strings representing all fields to obfuscate"""
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+            """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ Constructor method """
+        """Initialize the formatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = list(fields)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Filters values in incoming log records using filter_datum """
+        """Format the log record"""
         return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
-
-
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """Returns a logger object"""
+    """Return a logger with a redacting formatter"""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
-    logger.addHandler(handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
     return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to a database"""
-    connector = mysql.connector.connect(
-        user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
-        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
-        database=os.environ.get("PERSONAL_DATA_DB_NAME"))
-    return connector
+    """Return a connection to the database"""
+    db_conn = mysql.connector.connect(
+        host=os.environ.get('PERSONAL_DATA_DB_HOST'),
+        user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
+        passwd=os.environ.get('PERSONAL_DATA_DB_PASSWORD'),
+        database=os.environ.get('PERSONAL_DATA_DB_NAME')
+    )
+    return db_conn
 
 
-def main():
-    """Obtains a database connection using get_db and retrieves all rows in
-    the users table and display each row under a filtered format"""
+def main() -> None:
+    """Fetches and returns rows in users table"""
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
+    cursor.execute("SELECT * FROM users")
     logger = get_logger()
+    fields = [field[0] for field in cursor.description]
     for row in cursor:
-        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, PII_FIELDS))
-        logger.info(str_row)
+        str_row = "".join("{}={}; ".format(k, v)
+                          for k, v in zip(fields, row))
+        logger.info(str_row.strip())
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
